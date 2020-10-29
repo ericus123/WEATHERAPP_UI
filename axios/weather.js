064760c2e7623cd8cf75d03ab64d11bf277d9745
amani <amaniericus@gmail.com>
2020-10-29 22:04:42 +0200

    request functionality

--- axios/weather.js
+++ axios/weather.js
@@ -14,3 +14,15 @@ const timeField = document.querySelector('.time')
 dateField.innerText = currentDate;
 var ampm = date.getHours() >= 12 ? 'PM' : 'AM';
 timeField.innerText = `${date.getHours()}:${date.getMinutes()} ${ampm}`;
+let city;
+const sendBtn = document.querySelector(".butt")
+sendBtn.addEventListener('click', (e)=> {
+  e.preventDefault()
+  city = document.querySelector('.city').value
+  axios.get(`https://weatherappericus123.herokuapp.com/weather/${city}`).then((res) => {
+    console.log(res);
+    }).catch((err)=>{
+     console.log(err.res);
+    })
+})
+
